c25187c685d1f7a9772aaacb6a6caff7ada489f8
ultramarichka <maryna.sokolova11@gmail.com>
2018-03-01 14:26:58 +0100

    make 5 tables for 5 countries

--- js/index.js
+++ js/index.js
@@ -1,43 +1,51 @@
 $(document).ready(function(){
-  var response = "kukuc";
-  function successCallback(response){
-    
-    $("#example-table").tabulator({
-		  height:605, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-		  layout:"fitColumns", //fit columns to width of table (optional)
-		  columns:[ //Define Table Columns
-		      {title:"Logo", field:"image", width:150, formatter:"image", align:"center"},
-		      {title:"Brand name", field:"brand_name", align:"center"},
-		      {title:"Category name", field:"category_name", align:"center"},
-		      {title:"Country", field:"country", align:"center"},
-          {title:"producer", field:"producer", align:"center", width:"20"},
-          {title:"brand_id", field:"brand_id", align:"center", width:"20"},
-          {title:"master_id", field:"master_id", align:"center", width:"20"},
-          {title:"select_id", field:"select_id", align:"center", width:"20"},
-          {title:"row_id", field:"row_id", align:"center"},
-				],
-		}); 
-		//define some sample data
-
-    var tabledata = [];
-    for (var i = 0; i<response.length-1; i++){
-      console.log("i", i, response[i]);
-			tabledata.push(
-					{id:i, image:response[i]["logo"], brand_name:response[i]["brand_name"], category_name:response[i]["category_name"], country:response[i]["country"], producer:response[i]["producer"] , brand_id:response[i]["brand_id"], master_id:response[i]["brand_id"], select_id:response[i]["select_id"], row_id:response[i]["row_id"]}
-      );
-      
-    }
-		//load sample data into the table
-		$("#example-table").tabulator("setData", tabledata);
-
-  }
-
+  var response;// = [resp1, resp2, resp3, resp4, resp5];
   function errorFunc(){
 	  console.log("error");
 	}
-  var url = "http://ap.valicon.net/bsit/interview";
-  response = $.post(url,'{"Code":"SVN"}', successCallback);
-  response.fail(errorFunc);
 
+  var countries = {"SVN":"#table_svn", 
+                   "HRV":"#table_hrv", 
+                   "BIH":"#table_bih", 
+                   "SRB":"#table_srb",
+                   "MKD":"#table_mkd"};
+
+  for (let key in countries){
+		function successCallback(response){
+		   
+		  $(countries[key]).tabulator({
+				height:605, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+				layout:"fitColumns", //fit columns to width of table (optional)
+				columns:[ //Define Table Columns
+				    {title:"Logo", field:"image", width:150, formatter:"image", align:"center"},
+				    {title:"Brand name", field:"brand_name", align:"center"},
+				    {title:"Category name", field:"category_name", align:"center"},
+				    {title:"Country", field:"country", align:"center"},
+		        {title:"producer", field:"producer", align:"center"},
+		        {title:"brand_id", field:"brand_id", align:"center"},
+		        {title:"master_id", field:"master_id", align:"center"},
+		        {title:"select_id", field:"select_id", align:"center"},
+		        {title:"row_id", field:"row_id", align:"center"},
+					],
+			}); 
+			//define some sample data
+
+		  var tabledata = [];
+		  for (var i = 0; i<response.length-1; i++){
+				tabledata.push(
+						{id:i, image:response[i]["logo"], brand_name:response[i]["brand_name"], category_name:response[i]["category_name"], country:response[i]["country"], producer:response[i]["producer"] , brand_id:response[i]["brand_id"], master_id:response[i]["brand_id"], select_id:response[i]["select_id"], row_id:response[i]["row_id"]}
+		    );
+		  }
+			//load sample data into the table
+			$(countries[key]).tabulator("setData", tabledata);
+		}
+    /*---------END of the successCallback--------*/
+
+		
+		var url = "http://ap.valicon.net/bsit/interview";
+    var parameter = '{"Code":"' + key +'"}';
+		response = $.post(url, parameter, successCallback);
+		response.fail(errorFunc);
+  }
   
 });
